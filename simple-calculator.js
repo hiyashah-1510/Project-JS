e8efe169620ef9a8451644ab17ade44c7734faf4
Hiya Shah <hiyashah006@gmail.com>
2025-07-31 15:16:59 +0530

    Add files via upload

---
+++ simple-calculator.js
@@ -0,0 +1,7 @@
+let a = 10, b = 20;
+
+console.log(`Addition of ${a} and ${b} is : ${a + b}`);
+console.log(`Subtraction of ${a} and ${b} is : ${a - b}`);
+console.log(`Multiplication of ${a} and ${b} is : ${a * b}`);
+console.log( (b == 0) ? (`Number will not divided by zero`) : (`Division of ${a} and ${b} is : ${a / b}`));
+console.log(`Modulas of ${a} and ${b} is : ${a % b}`);
